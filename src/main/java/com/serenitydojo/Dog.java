c3e1a638cd8921bda9435d865eea2251831b27b0
dzarkV <dfvargasp@unal.edu.co>
2023-12-19 15:24:59 -0500

    Dog methods and static fields

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -1,11 +1,14 @@
 package com.serenitydojo;
 
-public class Cat {
+public class Dog {
     private String name;
     private String favoriteToy;
     private int age;
 
-    public Cat(String name, String favoriteToy, int age) {
+    private static final String DOG_NOISE = "Woof";
+    private boolean isFed;
+
+    public Dog(String name, String favoriteToy, int age) {
         this.name = name;
         this.favoriteToy = favoriteToy;
         this.age = age;
@@ -33,5 +36,17 @@ public class Cat {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String makeNoise() {
+        return DOG_NOISE;
+    }
+
+    public void feed() {
+        this.isFed = true;
+    }
+
+    public boolean isFed(){
+        return this.isFed;
     }
 }
